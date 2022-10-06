19041b7c6a3ae063a5373bebd8319e9a4cbe888d
dmacattack <dan.b.macdonald@gmail.com>
2022-10-06 10:50:57 -0400

    * create the mnt dirs before the partitions are created to ensure the system can mount correctly * update the backlight to 20kHz * add function file system to linux to enable adbd * remove alsa, bluez packages - fluke has no audio * add fonts and update qt packages to match solidruns config * update the fluke boot script to turn on the usb * bring in the S50usbdevice script * remove the old mount usb script

--- support/scripts/imx6/post-build.sh
+++ support/scripts/imx6/post-build.sh
@@ -6,3 +6,8 @@
 BOARD_DIR="$(dirname $0)"
 
 install -m 0644 -D $BOARD_DIR/extlinux.conf $BINARIES_DIR/extlinux.conf
+
+################################################
+### create mnt directories for the partitions ##
+################################################
+mkdir -p $TARGET_DIR/mnt/app $TARGET_DIR/mnt/appdata $TARGET_DIR/mnt/userdata
